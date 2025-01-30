f81b5fea2d9102fceaded95080ef5d66b3d10e72
afifShyam <afifaxelo12@gmail.com>
2025-01-30 15:57:41 +0800

    remove comment lah

--- app/src/main/java/com/example/todoappkotlin/AppModule.kt
+++ app/src/main/java/com/example/todoappkotlin/AppModule.kt
@@ -13,7 +13,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-//lah
     @Provides
     @Singleton
     fun provideDatabase(@ApplicationContext context: Context): TodoDatabase {
